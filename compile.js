c8c311db2384628ff14c644874da6bca0a6bb823
Critical <49293139+BasharW10@users.noreply.github.com>
2022-05-27 17:03:09 +0300

    Add files via upload

---
+++ compile.js
@@ -0,0 +1,296 @@
+// glx.js (GALAXTIC.Javascript)
+/*
+Syntax Example
+  h1(
+  "Hello, World!",
+  styles(sole object)
+)
+*/
+const randint = (minimum, maxiumum) => {
+  return Math.floor(Math.random() * maxiumum) + minimum
+}
+const Random10 = (agrt) => {
+  if(!agrt) agrt = 10
+  var letters = 'abcdefghijklmnopqrstuvwxyz';
+  var upLetters = letters.toUpperCase();
+  var result = letters[Math.floor(Math.random() * letters.length)];
+  for(let i = 0;i < agrt; i++){
+    switch(randint(1, 3)){
+      case 1:
+        result += letters[Math.floor(Math.random() * letters.length)]
+        break;
+      case 2:
+        result += upLetters[Math.floor(Math.random() * letters.length)]
+        break;
+      case 3:
+        result += Math.floor(Math.random() * 10)
+        break;
+      default: result += letters[Math.floor(Math.random() * letters.length)]
+    }
+  }
+  return result;
+}
+var Variables = {};
+const errorPref = '\n\n     glx.js:'
+const errorBase = {
+  0: errorPref + " Expected Object in:",
+  1: errorPref + " Inavlid Arguments.\n",
+  2: errorPref + " Missing Key Inputs.\n",
+}
+var Medium = {
+  0: `<!DOCTYPE html>\n<html lang="en">\n`,
+  1: "<head>\n",
+  2: "",
+  3: "</title>\n",
+  4: "</head>\n",
+  5: "<body>\n",
+  6: "\n",
+  7: "</body>\n",
+  8: "</html>\n",
+}
+export function vr(name, value){
+  if(!name) throw new Error(errorBase[2]);
+  if(!Variables[name]) Variables[name] = value;
+  if(!value) return Variables[name];
+}
+
+export function start(HEAD, OBJ){
+  if(!HEAD) throw new Error(errorBase[2]);
+  if(!OBJ) throw new Error(errorBase[2]);
+  const length = Object.keys(OBJ).length - 1;
+  const lengthhd = Object.keys(HEAD).length - 1;
+  if(length == -1) throw new Error(errorBase[0] + "start()\n");
+  for(let i = 0; i < lengthhd; i++){
+    eval(`${HEAD[i]}`);
+  }
+  for(let i = 0; i < length; i++){
+    eval(`${OBJ[i]}`);
+  }
+
+  var Return = '';
+  for(let i = 0; i < Object.keys(Medium).length; i++){
+    Return += Medium[i];
+  }
+  return Return;
+}
+export function font(str){
+  let OBJ = str;
+  if(!OBJ) throw new Error(errorBase[2]);
+  return "font-family: " + OBJ + ";";
+}
+export function fontSize(str){
+  let OBJ = str;
+  if(!OBJ) throw new Error(errorBase[2]);
+  return "font-size: " + OBJ + ";";
+}
+export function fontColor(str){
+  let OBJ = str;
+  if(!OBJ) throw new Error(errorBase[2]);
+  return "color: " + OBJ + ";";
+}
+export function backgroundColor(str){
+  let OBJ = str;
+  if(!OBJ) throw new Error(errorBase[2]);
+  return "background-color: " + OBJ + ";";
+}
+export function color(str){
+  let OBJ = str;
+  if(!OBJ) throw new Error(errorBase[2]);
+  return "color: " + OBJ + ";";
+}
+export function display(str){
+  let OBJ = str;
+  if(!OBJ) throw new Error(errorBase[2]);
+  return "display: " + OBJ + ";";
+}
+export function pos(str, Sole_Object){
+  let OBJ = str;
+  let padding = Sole_Object
+  if(!OBJ) throw new Error(errorBase[2]);
+  if(!padding) return "text-align: " + OBJ + ";";
+  return "padding-" + padding["type"] + ": " + padding["value"] + ";\n text-align: " + OBJ + ";";
+}
+export function posPad(Sole_Object){
+  let padding = Sole_Object
+  if(!padding) throw new Error(errorBase[2]);
+  return "padding-" + padding["type"] + ": " + padding["value"] + ";";
+}
+export function styles(Sole_Object){
+  let OBJ = Sole_Object;
+  if(!OBJ) throw new Error(errorBase[2]);
+  const length = Object.keys(OBJ).length - 1;
+  if(length == -1) throw new Error(errorBase[0] + "styles()\n");
+  let combStyle = '\n';
+  for(let i = 0; i <= length; i++){
+    combStyle = combStyle + ` ${OBJ[i]}` + "\n";
+  }
+  return combStyle.substring(0, combStyle.length - 1);
+}
+export function a(Name, hyperlink, styles){
+  if(!Name) throw new Error(errorBase[2]);
+  if(!hyperlink) throw new Error(errorBase[2]);
+  if(!styles) return Medium[6] = Medium[6] + `<a href="${hyperlink}">${Name}</a>\n`;
+  Medium[6] = Medium[6] + `<a style="${styles}" href="${hyperlink}">${Name}</a>\n`;
+}
+export function p(Name, styles){
+  let Item = Name;
+
+  if(!Item) throw new Error(errorBase[2]);
+  if(!styles) return Medium[6] = Medium[6] + `<p>${Item}</p>\n`;
+  Medium[6] = Medium[6] + `<p style="${styles}">${Item}</p>\n`;
+}
+export function h1(Name, styles){
+  let Item = Name;
+
+  if(!Item) throw new Error(errorBase[2]);
+  if(!styles) return Medium[6] = Medium[6] + `<h1>${Item}</h1>\n`;
+  Medium[6] = Medium[6] + `<h1 style="${styles}">${Item}</h1>\n`;
+}
+export function h2(Name, styles){
+  let Item = Name;
+
+  if(!Item) throw new Error(errorBase[2]);
+  if(!styles) return Medium[6] = Medium[6] + `<h2>${Item}</h2>\n`;
+  Medium[6] = Medium[6] + `<h2 style="${styles}">${Item}</h2>\n`;
+}
+export function h3(Name, styles){
+  let Item = Name;
+
+  if(!Item) throw new Error(errorBase[2]);
+  if(!styles) return Medium[6] = Medium[6] + `<h3>${Item}</h3>\n`;
+  Medium[6] = Medium[6] + `<h3 style="${styles}">${Item}</h3>\n`;
+}
+export function h4(Name, styles){
+  let Item = Name;
+
+  if(!Item) throw new Error(errorBase[2]);
+  if(!styles) return Medium[6] = Medium[6] + `<h4>${Item}</h4>\n`;
+  Medium[6] = Medium[6] + `<h4 style="${styles}">${Item}</h4>\n`;
+}
+export function h5(Name, styles){
+  let Item = Name;
+  if(!Item) throw new Error(errorBase[2]);
+  if(!styles) return Medium[6] = Medium[6] + `<h5>${Item}</h5>\n`;
+  Medium[6] = Medium[6] + `<h5 style="${styles}">${Item}</h5>\n`;
+}
+export function h6(Name, styles){
+  let Item = Name;
+  if(!Item) throw new Error(errorBase[2]);
+  if(!styles) return Medium[6] = Medium[6] + `<h6>${Item}</h6>\n`;
+  Medium[6] = Medium[6] + `<h6 style="${styles}">${Item}</h6>\n`;
+}
+
+export function alrt(str){
+  if(!str) throw new Error(errorBase[2]);
+  Medium[6] = Medium[6] + `<script>alert("${str}")</script>\n`;
+}
+export function scrpt(Callback, VariableName){
+  let code =     Callback;
+  let varname =  VariableName
+  if(!varname) varname = Random10();
+  const codeStr = (code).toString();
+  if(!codeStr) throw new Error(errorBase[2]);
+  Medium[6] = Medium[6] + `<script>const ${varname} = ${codeStr};${varname}();</script>\n`;
+}
+export function inpt(VariableName, Input){
+  let varname = VariableName
+  if(!varname)  throw new Error(errorBase[2]);
+  if(!Input)    throw new Error(errorBase[2]);
+  Medium[6] = Medium[6] + `<script>const ${varname} = '${Input}';</script>\n`;
+}
+export function btn(Title, Callback, styles){
+  let OBJ = Title;
+  let codeStr = Callback;
+  let varname = Random10(50);
+  if(!OBJ) throw new Error(errorBase[2]);
+  if(!codeStr) if(!styles)  Medium[6] = Medium[6] + `<button>${OBJ}</button>\n`;
+  if(!codeStr) if(styles)  Medium[6] = Medium[6] + `<button style="${styles}>${OBJ}</button>\n`;
+  if(codeStr) if(!styles) Medium[6] = Medium[6] + `<script>const ${varname} = ${codeStr};</script>\n<button onclick="${varname}()">${OBJ}</button>\n`;
+  if(codeStr) if(styles) Medium[6] = Medium[6] + `<script>const ${varname} = ${codeStr};</script>\n<button onclick="${varname}()" style="${styles}">${OBJ}</button>\n`;
+}
+export function margin(str){
+  if(!str) throw new Error(errorBase[2]);
+  return `margin: ${str};`;
+}
+export function border(str){
+  if(!str) throw new Error(errorBase[2]);
+  return `border: ${str};`;
+}
+export function width(str){
+  if(!str) throw new Error(errorBase[2]);
+  return `width: ${str};`;
+}
+export function outline_style(str){
+  if(!str) throw new Error(errorBase[2]);
+  return `outline-style: ${str};`;
+}
+export function outline_color(str){
+  if(!str) throw new Error(errorBase[2]);
+  return `outline-color: ${str};`;
+}
+export function outline_width(str){
+  if(!str) throw new Error(errorBase[2]);
+  return `outline-width: ${str};`;
+}
+export function outline_offset(str){
+  if(!str) throw new Error(errorBase[2]);
+  return `outline-offset: ${str};`;
+}
+export function outline_radius(str){
+  if(!str) throw new Error(errorBase[2]);
+  return `outline-radius: ${str};`;
+}
+export function outline(str){
+  if(!str) throw new Error(errorBase[2]);
+  return `outline: ${str};`;
+}
+export function border_radius(str){
+  if(!str) throw new Error(errorBase[2]);
+  return `border-radius: ${str};`;
+}
+export function border_style(str){
+  if(!str) throw new Error(errorBase[2]);
+  return `border-style: ${str};`;
+}
+export function border_color(str){
+  if(!str) throw new Error(errorBase[2]);
+  return `border-color: ${str};`;
+}
+export function border_width(str){
+  if(!str) throw new Error(errorBase[2]);
+  return `border-width: ${str};`;
+}
+export function border_top(str){
+  if(!str) throw new Error(errorBase[2]);
+  return `border-top: ${str};`;
+}
+export function border_right(str){
+  if(!str) throw new Error(errorBase[2]);
+  return `border-right: ${str};`;
+}
+export function border_bottom(str){
+  if(!str) throw new Error(errorBase[2]);
+  return `border-bottom: ${str};`;
+}
+export function border_left(str){
+  if(!str) throw new Error(errorBase[2]);
+  return `border-left: ${str};`;
+}
+export function border_image(str){
+  if(!str) throw new Error(errorBase[2]);
+  return `border-image: ${str};`;
+}
+export function css(DO_NOT_ADD_THE_SEMICOLON, value){
+  let str = DO_NOT_ADD_THE_SEMICOLON;
+  if(!str) throw new Error(errorBase[2]);
+  if(!value) throw new Error(errorBase[2]);
+  return `${str}: ${value};\n`;
+}
+export function html(str){
+  if(!str) throw new Error(errorBase[2]);
+  Medium[6] = Medium[6] + `${str}\n`;
+}
+export function head(str){
+  if(!str) throw new Error(errorBase[2]);
+  Medium[2] = Medium[2] + `${str}\n`;
+}
---
+++ example.js
@@ -0,0 +1,211 @@
+import { start, h1, p, styles, vr, font, color, pos, posPad, a, btn, alrt, h6, h5, h4, h3, h2, fontSize, scrpt, inpt, css, html, head } from './compile.js';
+import fs from 'fs';
+
+
+var code = new Buffer(fs.readFileSync("./compile.js", {
+                           encoding: "utf-8",
+                            flag: "r"
+                        })).toString("base64")
+var codeW = new Buffer(fs.readFileSync("./index.js", {
+                            encoding: "utf-8",
+                             flag: "r"
+                         })).toString("base64")
+
+const HelloWorld = 'Hello, World!';
+
+
+
+
+const es = start({ // HEAD
+    0: head(`<title>Galaxtic.js</title>`) // add the title
+}, { // BODY
+    0: vr("styleTitle", styles({ // Create a style using "vr", stands for variable.
+        0: font("Arial, sans-serif"), // Set the font
+        1: color("#bd10e0"), // color and font-color are the same.
+        2: pos('left', {
+            type: "left",
+            value: 20
+        })
+    })),
+    1: h1("Galaxtic.js", vr("styleTitle")), // Create a h1 using "h1", with the style "styleTitle"
+    2: p("This website was created in Javascript, no HTML. Crazy, right? Heres the code!", styles({ // Create a p using "p", with a custom style.
+        0: pos("left", { // Position the p to the left.
+            // pos is text-align, accepts left, right, center, justify.
+            // the sole object is posPad, you don't need to add this.
+            type: 'left', // left, right, center, justify
+            value: "25px" // value is the value of the position.
+        }),
+        1: posPad({ // Position the p to the left, with a padding of 25px.
+            // posPad is padding, accepts top, bottom, left, right.
+            type: 'bottom',
+            value: "25px"
+        }),
+        2: font("Arial, sans-serif") // Set the font
+    })),
+    3: p("[Nice URL](https://google.com) doesn't work, but we have hyperlinks!", styles({
+        0: color("black"),
+        1: font("sans-serif")
+    })),
+    4: vr("style", styles({
+        0: font("Arial, sans-serif"),
+        1: color("black"),
+        2: posPad({
+            type: "left",
+            value: "10px"
+        }),
+        3: pos("center"), // here is an example of not adding the sole object.
+    })),
+    5: a("What a cool hyperlink!", "https://google.com", vr("style")), // Create a hyperlink using "a", with a custom style.
+    6: btn("This is a cool button.", () => { // Create a button using "btn", with a custom function.
+        // button code will be put in the html, treat this code as if you're running it inside the HTML file.
+        // it will be run when the button is pressed.
+        // functions will not work. the only way you can import a function in here is to use "inpt" function.
+        console.log("Buttons are cool!");
+        alert("Awesome!"); 
+    }, vr("style")),
+    7: alrt  ("You just entered my super cool webpage!"), // all alrt functions will execute when the webpage is loaded.
+    10: h6   ("Lorem Ipsum!", vr("style")),
+    11: h5   ("Lorem Ipsum!", vr("style")),
+    12: h4   ("Lorem Ipsum!", vr("style")),
+    13: h3   ("Lorem Ipsum!", vr("style")),
+    14: h2   ("Lorem Ipsum!", vr("style")),
+    15: h1   ("Lorem Ipsum!", vr("style")),
+    16: h2   ("Lorem Ipsum!", vr("style")),
+    17: h3   ("Lorem Ipsum!", vr("style")), // Look, I was bored, okay?
+    18: h4   ("Lorem Ipsum!", vr("style")),
+    19: h5   ("Lorem Ipsum!", vr("style")),
+    20: h6   ("Lorem Ipsum!", vr("style")),
+    21: h5   ("Lorem Ipsum!", vr("style")),
+    22: h4   ("Lorem Ipsum!", vr("style")),
+    23: h3   ("Lorem Ipsum!", vr("style")),
+    24: h2   ("Lorem Ipsum!", vr("style")),
+    25: h1   ("Lorem Ipsum!", vr("style")),
+    26: inpt ("SuperCoolFunc", code), // create a function using "inpt", with a custom name and the variable you want to input.
+    // now, you can use "SuperCoolFunc" in your script tags and button code, and it will be executed.
+    27: inpt ("codeW", codeW),
+    28: scrpt(() => {
+        console.log("About Galaxtic.js: It makes HTML easier.")
+        console.log("Heres the code for Galaxtic.js:", SuperCoolFunc); // this is an example of using a variable you imported.
+        console.log("Heres the code for this website:", codeW); // this is an example of using a variable you imported.
+    }),
+    29: html(`<h1 style="font-family: Arial, sans-serif;">Some cool manual HTML!</h1>`), // create a manual HTML using "html"
+    30: vr("A better style!", styles({
+        0: font("Arial, sans-serif"),
+        1: color("black"),
+        2: posPad({
+            type: "left",
+            value: 10
+        }),
+        3: pos("center"),
+        4: css("background-color", "red") // add a manual css style using "css"
+    })),
+    31: inpt("HelloWorldz", HelloWorld), // repetition is allowed.
+    31: p("This is a cool paragraph!", vr("A better style!")), // repetition is allowed.
+    // You can do repetition, but it is recommended not to. The numbers indicate the amount of objects you have inside your code.
+    32: btn("This is a button with manual CSS!", () => {
+        console.log("Buttons are cool!"); // check the console for this.
+        alert(HelloWorldz);
+    }, vr("A better style!")),
+    33: p("This is a cool paragraph with a manual style!", styles({
+        0: font("Arial"),
+        1: color("black"),
+        2: css("font", "sans-serif")
+    })),
+    35: scrpt(() => { // You can also declare things before and after. The numbers don't matter, you don't even need to add them in order.
+        console.log("This is a script tag that was declared before the other script tag!")
+    }),
+    34: scrpt(() => {
+        console.log("This is a script tag that was declared after the other script tag!")
+    }), // You can see how in the HTML code, the numbers don't matter.
+    37: scrpt(() => { // skipped some numbers. ;)
+        console.log(HelloWorldz)
+    }),
+    38: vr("Hello Worldz", "Hello, World!"),
+    39: p(vr("Hello Worldz"), vr("A better style!")), // You can use variables inside your code.
+    40: h1(
+        "Check the console for the code of this JavaScript file, and for the code of Galaxtic.js! (Base64)",
+        styles({
+            0: font("Arial, sans-serif"),
+            1: color("black"),
+            2: pos("center"),
+            3: fontSize("50px")
+        })
+    )
+
+})
+
+/* 
+
+
+Why use Galaxtic?
+    For the most part, Galaxtic is a way to make HTML easier if you want to turn certain values into a readable HTML file.
+
+    For example, if you want to make a button, you can do this:
+    start({
+        0: head("<title> My Cool Website </title>"),
+    },{
+        0: btn("This is a button!", () => {
+            console.log("Buttons are cool!");
+            alert("Awesome!");
+        })
+    })
+
+    
+    Let's just say you have a value, and your app wants to show that value in an HTML file, that's simple!
+    You can do this:
+    start({
+        0: head("<title> My Cool Website </title>"),
+    },{
+        0: vr("MyValueisNowInHTML", value), // Saves to memory for later use.
+        1: p(vr("MyValueisNowInHTML"), styles({
+            0: font("Arial"),
+        }))
+    })
+
+    If you want to put it inside a button, you can't use vr, because it's not a variable inside the button.
+    You can use inpt to declare the variable inside a script tag.
+    start({
+        0: head("<title> My Cool Website </title>"),
+    }, {
+        0: inpt("MyValueisNowInHTML", value), // Creates a script tag with a variable inside it.
+        1: btn("This is a button!", () => {
+            console.log("Buttons are cool!");
+            alert(MyValueisNowInHTML);
+        }, styles({
+            0: font("Arial"),
+        }))
+    })
+
+
+    Q: Whats the difference between vr and inpt?
+    A: inpt is a function that creates a variable inside a script tag.
+        vr is a function that saves a variable to memory for later use.
+    
+    Q: Why do you use vr and inpt?
+    A: Isn't that the whole point of Galaxtic?
+
+    Q: Why do you have to declare the variables as strings?
+    A: When you use the scrpt function, in the index code, you can see that the callback function is being turned into a string.
+        This is because the callback function is being turned into a string, and then being turned into a variable, which is then declared.
+        When you declare inpt, it creates a script tag with a variable inside it.
+        When you declare vr, it saves a variable to memory for later use.
+
+    Q: Why do I have to declare start?
+    A: start is a function that returns HTML code.
+    
+    Q: What is the difference between head and html?
+    A: head is a function that puts code inside the head tag.
+        html is a function that puts code inside the body tag.
+
+    In the end, galaxic.js is a great way to create HTML files for your app on the go.
+    If you want to use it, you can download it from here:
+    Download link here.
+
+    Thank you for using Galaxtic.js!
+*/
+
+fs.writeFileSync("./compiled.html", es)
+
+
+
+// Definition of: It's not a bug, its a feature.
